fd4def6307fd88d0bd0ca5cf9f9474182cc848cc
sunamoto-mikiya <miki38gtr@gmail.com>
2023-02-24 17:07:43 +0900

    タスクの追加処理実装

--- lib/main.dart
+++ lib/main.dart
@@ -130,7 +130,26 @@ class _ToDoListPageState extends State<ToDoListPage> {
   List<String> toDoList = [];
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('タスク一覧画面')),
+      appBar: AppBar(
+        title: Text('タスク一覧画面'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () async {
+              // ログアウト処理
+              // 内部で保持しているログイン情報等が初期化される
+              // （現時点ではログアウト時はこの処理を呼び出せばOKと、思うぐらいで大丈夫です）
+              await FirebaseAuth.instance.signOut();
+              // ログイン画面に遷移＋チャット画面を破棄
+              await Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) {
+                  return LoginPage();
+                }),
+              );
+            },
+          ),
+        ],
+      ),
       body: ListView.builder(
         itemCount: toDoList.length,
         itemBuilder: (context, index) {
